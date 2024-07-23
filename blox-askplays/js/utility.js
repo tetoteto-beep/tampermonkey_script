e9bbe72b994628c96d5d3054fafb583883c9edfa
tetoteto <sasa24games@gmail.com>
2024-07-23 15:53:46 +0900

    修正途中

---
+++ blox-askplays/js/utility.js
@@ -0,0 +1,28 @@
+// 余りが正の数となるmod関数
+function mod(a, b) {
+    return ((a % b) + b) % b;
+}
+
+function simulateKeyPress(key) {
+    const keyEventInit = {
+        key: key,
+        code: `Key${key.toUpperCase()}`,
+        keyCode: key.toUpperCase().charCodeAt(0),
+        which: key.toUpperCase().charCodeAt(0),
+        bubbles: true,
+        cancelable: true
+    };
+
+    document.dispatchEvent(new KeyboardEvent('keydown', keyEventInit));
+    document.dispatchEvent(new KeyboardEvent('keyup', keyEventInit));
+}
+
+// 文字列をランダムにシャッフルする関数
+function shuffleString(str) {
+    const arr = str.split('');
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr.join('');
+}
