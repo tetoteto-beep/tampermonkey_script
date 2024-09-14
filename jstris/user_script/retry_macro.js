5900215c4f38922ee798916e617a472c3ce3071c
tetoteto <sasa24games@gmail.com>
2024-09-15 04:11:09 +0900

    jstrisのリトライ処理マクロを変更

--- jstris/user_script/retry_macro.js
+++ jstris/user_script/retry_macro.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         jstris AI対戦時のリトライ処理マクロ
+// @name         F4キーを押下することで対戦をリトライする
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  jstris上でColdClearなどのaiと対戦する際、rボタンを押下することでリトライできるようにする。
+// @version      1.0
+// @description  F4キーを押下することで対戦をリトライする処理。jstrisでは設定からリトライキーを変更できる機能があるが、うまく動作しないので代替の処理。
 // @author       You
 // @match        https://jstris.jezevec10.com/*
 // @grant        none
@@ -11,64 +11,26 @@
 (function() {
     'use strict';
 
-    let rCount = 0;
+    function simulateF4KeyPress() {
+        // キーボードイベントの生成
+        let event = new KeyboardEvent('keydown', {
+            key: 'F4',
+            keyCode: 115, // F4のキーコード
+            which: 115, // F4のキーコード
+            code: 'F4',
+            bubbles: true, // イベントがバブリングするか
+            cancelable: true, // イベントをキャンセルできるか
+        });
+
+        // ターゲット要素にイベントをディスパッチ
+        document.dispatchEvent(event);
+    }
 
     document.addEventListener('keydown', function(event) {
         if (event.key === 'r') {
-            simulateActions();
-            rCount++;
-            //sendChatMessage(rCount);
+            // F4キー押下をシミュレート
+            simulateF4KeyPress();
         }
     });
 
-    function simulateActions() {
-        // canvas要素をクリックしてフォーカスを設定
-        //const canvas = document.querySelector('#myCanvas');
-        //if (canvas) {
-        //    canvas.focus();  // フォーカスを与える
-        //    canvas.click();  // クリックをシミュレート
-        //}
-
-        // 上ボタン（ハードドロップが上ボタンになっている想定）を20回押す
-        for (let i = 0; i < 20; i++) {
-            simulateKeyPress();
-        }
-
-        // 開始ボタンを押す
-        const startButton = document.querySelector('#res[data-text="開始"]');
-        if (startButton) {
-            startButton.click();
-        }
-    }
-
-    function simulateKeyPress() {
-        const event = new KeyboardEvent('keydown', {
-            key: 'ArrowUp',
-            code: 'ArrowUp',
-            keyCode: 38,
-            which: 38,
-            bubbles: true
-        });
-        document.dispatchEvent(event);
-
-        const keyupEvent = new KeyboardEvent('keyup', {
-            key: 'ArrowUp',
-            code: 'ArrowUp',
-            keyCode: 38,
-            which: 38,
-            bubbles: true
-        });
-        document.dispatchEvent(keyupEvent);
-    }
-
-    // チャットメッセージを送信する関数
-    function sendChatMessage(count) {
-        const chatInput = document.getElementById('chatInput');
-        const sendButton = document.getElementById('sendMsg');
-
-        if (chatInput && sendButton) {
-            chatInput.value = `cnt:${count}`;
-            sendButton.click();
-        }
-    }
 })();
